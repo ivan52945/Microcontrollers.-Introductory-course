3a58eeb9283e8a23b0ebefcf5310c09784751935
ivan52945 <ivan52945@gmail.com>
2024-10-15 14:07:14 +0300

    change some info in prev project

--- 2. GPIO. Interrupts/Core/Src/button.c
+++ 2. GPIO. Interrupts/Core/Src/button.c
@@ -27,8 +27,45 @@ void init_button(GPIO_TypeDef* port, uint16_t pin)
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(port, &GPIO_InitStruct);
 
-	HAL_NVIC_SetPriority(EXTI9_5_IRQn, 3, 0);
-	HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+	switch(pin){
+		case GPIO_PIN_0:
+			HAL_NVIC_SetPriority(EXTI0_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+			break;
+		case GPIO_PIN_1:
+			HAL_NVIC_SetPriority(EXTI1_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+			break;
+		case GPIO_PIN_2:
+			HAL_NVIC_SetPriority(EXTI2_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+			break;
+		case GPIO_PIN_3:
+			HAL_NVIC_SetPriority(EXTI3_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+			break;
+		case GPIO_PIN_4:
+			HAL_NVIC_SetPriority(EXTI4_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+			break;
+		case GPIO_PIN_5:
+		case GPIO_PIN_6:
+		case GPIO_PIN_7:
+		case GPIO_PIN_8:
+		case GPIO_PIN_9:
+			HAL_NVIC_SetPriority(EXTI9_5_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+			break;
+		case GPIO_PIN_10:
+		case GPIO_PIN_11:
+		case GPIO_PIN_12:
+		case GPIO_PIN_13:
+		case GPIO_PIN_14:
+		case GPIO_PIN_15:
+			HAL_NVIC_SetPriority(EXTI15_10_IRQn, 3, 0);
+			HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+			break;
+	}
 }
 
 
